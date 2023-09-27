f7726ff478efefe2156277da6b83111a9677c004
luca.canali <lucacanali.dev@gmail.com>
2023-09-27 18:11:36 +0200

    add match_analyst.py

---
+++ match_analyst/match_analyst.py
@@ -0,0 +1,64 @@
+from chess import pgn
+import io
+import chess
+
+#al momento il codice è ottimizzato solo per le aperture.
+#TODO: analisi chiusure, analisi mosse
+
+#funzione presa da pgn_manager. Da sistemare la gestione dei moduli per importare la funzione
+def _readPGN(pgn_path):
+    pgn_file = open(pgn_path)
+    pgn_content = pgn_file.read()
+    pgn_file.close()
+
+    pgn_games = []
+    pgn = io.StringIO(pgn_content)
+    while True:
+        game = chess.pgn.read_game(pgn)
+        if game is None:
+            break
+        pgn_games.append(game)
+
+    return pgn_games
+
+def main(filename):
+    partite = _readPGN(filename)
+    aperture = {}
+    chiusure = {}
+    altre_chiusure = 0
+    mosse = {}
+
+    for partita in partite:
+        apertura = partita.headers.get("ECO")
+        chiusura = partita.headers.get("Result")
+        mosse = partita.mainline()
+
+        if apertura not in aperture:
+            aperture[apertura] = 0
+        aperture[apertura] += 1
+
+        if chiusura not in chiusure:
+            chiusure[chiusura] = 0
+        else:
+            chiusure[chiusura] += 1
+
+        if chiusura not in ["1-0", "0-1", "1/2-1/2"]:
+            altre_chiusure += 1
+
+    print("Aperture:")
+    aperture_sorted = sorted(aperture.items(), key=lambda x: x[1], reverse=True)
+    for apertura, frequenza in aperture_sorted:
+        frequenza = frequenza / len(partite)
+        tot = aperture.get(apertura, 0)
+        print(f"{apertura}: {frequenza * 100:.2f}%" + " " + str(tot))
+
+    print("Chiusure:")
+    for chiusura, frequenza in chiusure.items():
+        frequenza = frequenza / len(partite)
+        print(f"{chiusura}: {frequenza * 100:.2f}%")
+
+    print("Altre chiusure:", altre_chiusure)
+
+#per testare lo script modificare l'argomento della funzione con il percorso del PGN desiderato
+if __name__ == "__main__":
+    main("/Users/lucacanali/Desktop/all_pgn_koivisto_white.pgn")
