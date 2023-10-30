cd162e7145f120a1b53d21840a93320594d858b5
luca.canali <lucacanali.dev@gmail.com>
2023-10-30 11:45:34 +0100

    Update opening_analyst.py

--- tools/source_code/opening_analyst.py
+++ tools/source_code/opening_analyst.py
@@ -2,35 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pgn_manager import _readPGN
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-
-def on_button_search_file(text_box):
-    file = filedialog.askopenfilename()
-    text_box.delete(1.0, "end")
-    text_box.insert(1.0, file)
-
-def on_button_search_path(text_box):
-    percorso = filedialog.askdirectory()
-    text_box.delete(1.0, "end")
-    text_box.insert(1.0, percorso)
-
-'----------BETA----------'
-
-def calculate_win_percentage(opening, matches, tot_opening):
-    wins = 0
-    losses = 0
-    for match in matches:
-        if opening == match.headers.get("ECO"):
-            if match.headers.get("Result") == "1-0":
-                wins += 1
-            elif match.headers.get("Result") == "0-1":
-                losses += 1
-    if wins != 0:
-        return (wins * 100) / tot_opening
-    else:
-        return 0
-
 
 def main(filename):
     games = _readPGN(filename)
@@ -60,6 +31,7 @@ def main(filename):
     opening_sorted = sorted(ecos.items(), key=lambda x: x[1], reverse=True)
     x_data_usage = []
     y_data_usage = []
+    y_data_winning = []
     print("NUMBER OF GAMES:", len(games))
     print("NUMBER OF CHESS OPENING:", len(ecos))
     print("Opening:")
@@ -67,35 +39,32 @@ def main(filename):
         percentage_of_use = (frequence / len(games)) * 100
         percentage_of_wins = (wins_for_opening.get(eco, 0) / frequence) * 100
         name = opening_name.get(eco, "UNKNOWN")
-        x_data_usage.append(str(eco) + '\n' + str(name))
+        x_data_usage.append(str(eco))
         y_data_usage.append(percentage_of_use)
-        print(f"({eco}) {name}: {percentage_of_use:.2f}% (Percentage of win: {percentage_of_wins:.2f}%)" + " Number of use: " + str(frequence))
+        y_data_winning.append(percentage_of_wins)
+        print(f"({eco}) {name}: (Percentage of use: {percentage_of_use:.2f}%) (Percentage of win: {percentage_of_wins:.2f}%)" + " Number of use: " + str(frequence))
 
-    '''----------GRAPH----------'''
+    '''----------GRAPH USE----------'''
     df = pd.DataFrame({'Openings': x_data_usage, 'Percentage of use': y_data_usage})
     df_truncated = df.head(20)
     plt.bar(df_truncated['Openings'], df_truncated['Percentage of use'], width=0.8, color='green')
     plt.xticks(df_truncated['Openings'], size=8)
     plt.xlabel('Openings')
     plt.ylabel('Percentage of use')
-    plt.title('')
+    plt.title('Opening analyst')
 
     plt.show()
 
+    '''----------GRAPH WIN----------'''
+    df_win = pd.DataFrame({'Openings': x_data_usage, 'Percentage of win': y_data_winning})
+    df_win_truncated = df_win.head(20)
+    plt.bar(df_win_truncated['Openings'], df_win_truncated['Percentage of win'], width=0.8, color='green')
+    plt.xticks(df_win_truncated['Openings'], size = 8)
+    plt.xlabel('Openings')
+    plt.ylabel('Percentage of win')
+    plt.title('Opening analyst - win')
 
-# --------------------------------------------------------------------------------------------------------GUI--------------------------------------------------------------------------------------------------------
+    plt.show()
 
-root = tk.Tk()
-root.title("pgn_manager")
-root.geometry("580x280")
-
-button_search_file_csv_frame = ttk.Button(root, text = 'PGN file', 
-                                          command=lambda: on_button_search_file(text_box))
-text_box = tk.Text(root, width=50, height=1)
-button_analyze = ttk.Button(root, text = 'Analyze', command=lambda: main(text_box.get(1.0, 'end-1c')))
-#positioning button
-button_search_file_csv_frame.grid(row=0, column=0, padx=10, pady=10)
-button_analyze.grid(row=2, column=0, padx=10, pady=10)
-text_box.grid(row=0, column=1, padx=0, pady=10, sticky='ew')
-
-root.mainloop()
+main('/Users/lucacanali/Documents/GitHub/tirocinio_lucacanali/dataset/Koivisto_9_0_64-bit.bare.[3174].pgn (1)/Koivisto_9_0_64-bit.bare.[3174].pgn')
+## change main parameter to use the script
