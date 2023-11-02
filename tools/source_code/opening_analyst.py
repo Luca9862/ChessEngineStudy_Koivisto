2927bfffba14210aef03735f1e5d87422911e787
luca.canali <lucacanali.dev@gmail.com>
2023-11-02 11:46:56 +0100

    Update opening_analyst
    now you can export data in csv file and save the graphs in png files

--- tools/source_code/opening_analyst.py
+++ tools/source_code/opening_analyst.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pgn_manager import _readPGN
 import tkinter as tk
+import csv
 
 def main(filename):
+    csv_columns = ["eco_code", "percentage_of_use", "percentage_of_win", "number_of_use", "number_of_wins"]
+    csv_data = []
     games = _readPGN(filename)
+    wins = 0
+    loses = 0
+    draws = 0
     ecos = {}
     wins_for_opening = {}
     opening_name = {}
@@ -23,6 +29,11 @@ def main(filename):
             if eco not in wins_for_opening:
                 wins_for_opening[eco] = 0
             wins_for_opening[eco] += 1
+            wins += 1
+        elif result == "0-1":
+            loses += 1
+        elif result == "1/2-1/2":
+            draws += 1
 
         if eco not in opening_name:
             opening_name[eco] = opening
@@ -32,7 +43,16 @@ def main(filename):
     x_data_usage = []
     y_data_usage = []
     y_data_winning = []
+    wins_percentage = (wins/len(games)*100)
+    loses_percentage = (loses/len(games)*100)
+    draws_percentage = (draws/len(games)*100)
+    wins_percentage_formatted = "{:.2f}".format(wins_percentage)
+    loses_percentage_formatted = "{:.2f}".format(loses_percentage)
+    draws_percentage_formatted = "{:.2f}".format(draws_percentage)
     print("NUMBER OF GAMES:", len(games))
+    print("NUMBER OF WINS:", str(wins), '-', str(wins_percentage_formatted) + '%')
+    print("NUMBER OF LOSES:", str(loses), '-', str(loses_percentage_formatted)+ '%')
+    print("NUMBER OF DRAWS:", str(draws), '-', str(draws_percentage_formatted) + '%')
     print("NUMBER OF CHESS OPENING:", len(ecos))
     print("Opening:")
     for eco, frequence in opening_sorted:
@@ -43,6 +63,13 @@ def main(filename):
         y_data_usage.append(percentage_of_use)
         y_data_winning.append(percentage_of_wins)
         print(f"({eco}) {name}: (Percentage of use: {percentage_of_use:.2f}%) (Percentage of win: {percentage_of_wins:.2f}%)" + " Number of use: " + str(frequence))
+        csv_data.append([eco, percentage_of_use, percentage_of_wins, frequence, wins_for_opening.get(eco)])
+
+    output_csv = 'csv_exported.csv'
+    with open(output_csv, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(csv_columns)  # Scrivi i nomi delle colonne
+        writer.writerows(csv_data)  # Scrivi i dati
 
     '''----------GRAPH USE----------'''
     df = pd.DataFrame({'Openings': x_data_usage, 'Percentage of use': y_data_usage})
@@ -52,6 +79,7 @@ def main(filename):
     plt.xlabel('Openings')
     plt.ylabel('Percentage of use')
     plt.title('Opening analyst')
+    plt.savefig('use_graph.png')
 
     plt.show()
 
@@ -63,6 +91,7 @@ def main(filename):
     plt.xlabel('Openings')
     plt.ylabel('Percentage of win')
     plt.title('Opening analyst - win')
+    plt.savefig('win_graph.png')
 
     plt.show()
 
